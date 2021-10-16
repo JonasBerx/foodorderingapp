a4bba4ee681d35bac81acf24a8eddb30e7a526bf
Zhaosi Qu <git@chaos.run>
2021-10-17 00:16:39 +0300

    fix login function

--- dolt/views.py
+++ dolt/views.py
@@ -7,7 +7,7 @@ from dolt.models import User
 
 @app.route("/")
 def index():
-    return render_template("index.html"), 200
+    return render_template("index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -17,11 +17,19 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
+        # Check input values
+        if not username:
+            flash("Input invalid: Please enter a username")
+            redirect(url_for("login"))
+        elif not password:
+            flash("Input invalid: Please enter a password")
+            redirect(url_for("login"))
+
         # Get the user
         user = User.query.filter(User.username == username).first()
 
         # Check password hash
-        if user.validate_password(password):
+        if user and user.validate_password(password):
             login_user(user)
             flash("Login succeeded")
             return redirect(url_for("index"))
@@ -30,7 +38,7 @@ def login():
         flash("Invalid username or password")
         return redirect(url_for("login"))
 
-    return render_template("login.html"), 200
+    return render_template("login.html")
 
 
 @app.route("/logout")
@@ -39,3 +47,9 @@ def logout():
     logout_user()
     flash("Logout succeeded")
     return redirect(url_for("index"))
+
+
+@app.route("/settings", methods=["GET", "POST"])
+@login_required
+def settings():
+    return render_template("index.html")
