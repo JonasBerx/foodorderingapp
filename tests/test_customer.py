8ce02fe145b2be8f8e921d134cecb743c479ee6e
Zhaosi Qu <git@chaos.run>
2021-10-21 20:26:59 +0300

    [Test - Fail] add invalid order test

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -43,13 +43,19 @@ class DoltTestCaseCourier(unittest.TestCase):
 
     def test_order_food(self):
         self.mock_login()
-
         response = self.client.post("/order/new/1", follow_redirects=True)
         data = response.get_data(as_text=True)
         self.assertIn("Order created", data)
         self.assertIn("Restaurant 1", data)
         self.assertIn("Food 1", data)
         self.assertIn("Ongoing", data)
+
+    def test_invalid_order(self):
+        self.mock_login()
+        response = self.client.post("/order/new/2", follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn("Order created", data)
+        self.assertIn("Invalid request", data)
 
 
 if __name__ == '__main__':
