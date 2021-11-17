2badf0450cdf9ae77bb313998fd463c44589ec5c
Jonas Berx <joberx@ttu.ee>
2021-11-17 13:03:27 +0200

    f tests finished

--- functional_tests/fr7_accept_delivery_mission.py
+++ functional_tests/fr7_accept_delivery_mission.py
@@ -43,6 +43,45 @@ class FrAcceptDeliveryMission(unittest.TestCase):
         alert = driver.find_element_by_xpath("/html/body/div[1]")
         assert alert.text == 'Mission Accepted successfully'
 
+    # def test_take_non_existent_mission(self):
+    #     driver = self.driver
+    #     driver.get("http://127.0.0.1:5000/")
+    #     assert "ᗺolt Food" in driver.title
+    #     driver.get("http://127.0.0.1:5000/login")
+    #     username = driver.find_element_by_id("unm")
+    #     username.send_keys("cou")
+    #     password = driver.find_element_by_id("pwd")
+    #     password.send_keys("12345")
+    #     driver.find_element_by_name("submit").click()
+    #     alert = driver.find_element_by_xpath("/html/body/div[1]")
+    #     assert alert.text == 'Login succeeded'
+    #     try:
+    #         driver.get("http://127.0.0.1:5000/courier")
+    #         if driver.find_element_by_xpath("/html/body/div/div/text()").text == "Session in Progress":
+    #             True
+
+    #     except NoSuchElementException:
+    #         start_session = '''var xhr = new XMLHttpRequest();
+    #             xhr.open('POST', 'http://127.0.0.1:5000/courier/session/start', false);
+    #             xhr.setRequestHeader(
+    #                 'Content-type', 'application/x-www-form-urlencoded');
+
+    #             xhr.send();
+    #             return xhr.response;'''
+    #         driver.execute_script(start_session)
+    #     driver.get("http://127.0.0.1:5000/courier/missions")
+    #     # We assume that there is open missions
+    #     non_existent = '''var xhr = new XMLHttpRequest();
+    #             xhr.open('POST', 'http://127.0.0.1:5000/courier/missions/99999999/accept', false);
+    #             xhr.setRequestHeader(
+    #                 'Content-type', 'application/x-www-form-urlencoded');
+
+    #             xhr.send();
+    #             return xhr.response;'''
+    #     driver.execute_script(non_existent)
+    #     alert = driver.find_element_by_xpath("/html/body/div[1]")
+    #     assert alert.text == 'Invalid request'
+
     def tearDown(self):
         self.driver.close()
 
